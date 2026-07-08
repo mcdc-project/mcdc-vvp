67831cb75d27227002e5a18a9572222cc4c06ee2
ilhamv <variansyah.ilham@gmail.com>
2026-07-07 22:21:24 -0700

    fix indentation

---
+++ configs/user_config.py
@@ -0,0 +1,15 @@
+USER_CONFIG = {
+    "dane": {
+        "account": "orsu",
+        "queue": "pbatch",
+        "reservation": None,
+	"maestro_python": None,
+    },
+    "tuolumne": {
+        "account": "orsu",
+        "queue": "pbatch",
+        "reservation": None,
+        "maestro_python": "/g/g92/variansyah1/venv/tuolumne/maestrowf/bin/python",
+        "mcdc_python": "/g/g92/variansyah1/venv/tuolumne/mcdc/bin/python",
+    },
+}
